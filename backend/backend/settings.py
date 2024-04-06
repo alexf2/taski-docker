9c290d67ce3a43a0b6313e2f3368a4d2d0c1f9d2
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2024-04-06 18:42:04 +0300

    Добавление Git Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'afyandex.webhop.me', 'afyandex.name']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'afyandex.webhop.me']
 
 
 # Application definition
